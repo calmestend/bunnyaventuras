914774491425a957361770d265d25a578ad82deb
Barac Fabregat <barac.fabregat.gallegos@gmail.com>
2024-10-14 20:06:54 -0600

    Configure .env

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +10,8 @@ Route::post('/auth/register', [UserController::class, 'create']);
 Route::post('/auth/login', [UserController::class, 'login']);
 
 Route::post('/category', [CategoryController::class, 'create']);
+
+Route::post('/comment', [CommentController::class, 'create']);
+
+Route::post('/post', [PostController::class, 'create']);
+Route::get('/posts', [PostController::class, 'all']);
